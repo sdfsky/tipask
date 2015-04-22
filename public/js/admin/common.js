b12c88ab97ac42d8ed561805a786e444669796d9
sky_php <sky_php@qq.com>
2015-04-22 16:24:20 +0800

    添加后台用户管理

---
+++ public/js/admin/common.js
@@ -0,0 +1,26 @@
+/**
+ * Created by simon on 2015/4/22.
+ */
+
+
+$(function () {
+    //Enable iCheck plugin for checkboxes
+    //iCheck for checkbox and radio inputs
+    $('input[type="checkbox"]').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue'
+    });
+
+
+    //全部选中checkbox
+    $('.checkbox-toggle').on('ifChecked', function(event){
+        $("input[type='checkbox'][class!='checkbox-toggle']").iCheck('check');
+    });
+
+    //全部取消选中checkbox
+    $('.checkbox-toggle').on('ifUnchecked', function(event){
+        $("input[type='checkbox'][class!='checkbox-toggle']").iCheck('uncheck');
+    });
+
+
+});
---
+++ resources/views/admin/public/success.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 2015/4/22
+ * Time: 11:24
+ */ 
