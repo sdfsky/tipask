022ca046ca946d73412ac1cb47b99eac45bbb791
宋登峰 <sky_php@qq.com>
2016-05-19 23:02:45 +0800

    add new tipask

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->command('inspire');
     }
 }
