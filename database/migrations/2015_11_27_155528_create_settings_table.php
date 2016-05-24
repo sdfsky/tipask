5bd2c740646cd17ca518ce8250549420b61ab19b
宋登峰 <sky_php@qq.com>
2016-05-24 16:40:06 +0800

    remove setting data init

--- database/migrations/2015_11_27_155528_create_settings_table.php
+++ database/migrations/2015_11_27_155528_create_settings_table.php
@@ -17,7 +17,6 @@ class CreateSettingsTable extends Migration
             $table->text('value')->nullable();             //设置备注
         });
 
-        $this->initData();
 
     }
 
@@ -32,35 +31,5 @@ class CreateSettingsTable extends Migration
     }
 
 
-    private function initData()
-    {
-        $settings = [
-            ['name' => 'coins_adopted','value' => '0'],
-            ['name' => 'coins_answer','value' => '0'],
-            ['name' => 'coins_ask','value' => '0'],
-            ['name' => 'coins_login','value' => '0'],
-            ['name' => 'coins_register','value' => '20'],
-            ['name' => 'credits_adopted','value' => '20'],
-            ['name' => 'credits_answer','value' => '10'],
-            ['name' => 'credits_ask','value' => '0'],
-            ['name' => 'credits_login','value' => '10'],
-            ['name' => 'credits_register','value' => '20'],
-            ['name' => 'date_format','value' => 'Y-m-d'],
-            ['name' => 'time_diff','value' => '0'],
-            ['name' => 'time_format','value' => 'H:i'],
-            ['name' => 'time_friendly','value' => '1'],
-            ['name' => 'time_offset','value' => '8'],
-            ['name' => 'website_admin_email','value' => 'admin@tipask.com'],
-            ['name' => 'website_footer','value' => ''],
-            ['name' => 'website_header','value' => ''],
-            ['name' => 'website_icp','value' => '京ICP备13022057号'],
-            ['name' => 'website_name','value' => 'Tipask问答网'],
-            ['name' => 'website_url','value' => 'http://www.tipask.com'],
-        ];
 
-        DB::table('settings')->insert($settings);
-
-
-
-    }
 }
