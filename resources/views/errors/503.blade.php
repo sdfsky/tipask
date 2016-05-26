de24fb80117de17e65d0da91c62101806d6856cc
宋登峰 <sky_php@qq.com>
2016-05-26 22:37:26 +0800

    添加网站维护提示页面

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,14 +1,10 @@
 @extends('errors/layout')
 
-@section('title') 503 Service Unavailable @endsection
+@section('title') 网站维护中 @endsection
 
 
 @section('content')
 
-    <h1><i class="fa fa-exclamation-triangle orange"></i>  503 Service Unavailable</h1>
-    <p class="lead">The web server is returning an unexpected temporary error for {{ Setting()->get('website_url') }}.</p>
-    <p>
-        <a href="javascript:document.location.reload(true);" class="btn btn-default btn-lg text-center"><span class="green">Try This Page Again</span></a>
-    </p>
-
+    <h1><i class="fa fa-exclamation-triangle orange"></i> 网站维护中，请稍后再访问.</h1>
+    <p class="lead">我们正在对网站进行维护，维护期间暂时不能正常访问，抱歉！</p>
 @endsection
