48623c7f581f8d3158e8b019855e1bb72382af8c
sdf_sky <sky_php@qq.com>
2016-06-01 22:45:09 +0800

    update login.blade.php

--- resources/views/admin/account/login.blade.php
+++ resources/views/admin/account/login.blade.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>AdminLTE 2 | Log in</title>
+    <title>Tipask管理后台登陆</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     <!-- Bootstrap 3.3.2 -->
