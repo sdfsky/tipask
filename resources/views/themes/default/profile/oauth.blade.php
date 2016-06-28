e6dbeee03c2bd57f660ef933e6e8e895322b9223
宋登峰 <sky_php@qq.com>
2016-06-28 14:21:38 +0800

    add license

--- resources/views/themes/default/profile/oauth.blade.php
+++ resources/views/themes/default/profile/oauth.blade.php
@@ -37,26 +37,4 @@
             </div>
         </div>
     </div>
-
-    <div class="modal fade " id="register_license_modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                    <h4 class="modal-title text-center">{{ Setting()->get('website_name') }}服务条款</h4>
-                </div>
-                <div class="modal-body">
-                    <div style="height: 450px;overflow:scroll;">
-                        {!! Setting()->get('register_license','') !!}
-                    </div>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
 @endsection
