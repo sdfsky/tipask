fd6d5e622d23705a0d1a509e317c6066c0eda8f7
宋登峰 <sky_php@qq.com>
2016-07-07 10:01:29 +0800

    调整后台站点url验证规则

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -16,7 +16,7 @@ class SettingController extends AdminController
         $validateRules = [
             'website_name' => 'required|max:128',
             'website_slogan' => 'sometimes|max:128',
-            'website_url' => 'required|active_url',
+            'website_url' => 'required|url',
             'website_icp' => 'sometimes|max:128',
             'website_cache_time' => 'sometimes|digits_between:0,8640',
             'website_admin_email' => 'required|email',
