4e1ad108f763b6e657b6cbbf2a3d5009b741497a
宋登峰 <sky_php@qq.com>
2016-07-21 22:46:16 +0800

    remove sf images

---
+++ app/Http/Controllers/WeiXin/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sdf_sky
+ * Date: 16/7/21
+ * Time: 上午11:01
+ */
+
+namespace app\Http\Controllers\WeiXin;
+
+
+class IndexController
+{
+
+}
---
+++ resources/views/weixin/index/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sdf_sky
+ * Date: 16/7/21
+ * Time: 上午11:04
+ */
