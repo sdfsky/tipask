62a25c20ca718d6c91de983d769a15339a46b768
宋登峰 <sky_php@qq.com>
2016-07-25 16:01:04 +0800

    add font property

--- config/purifier.php
+++ config/purifier.php
@@ -24,7 +24,7 @@ return [
     'settings'  => [
         'default' => [
             'HTML.Doctype'             => 'XHTML 1.0 Transitional',
-            'HTML.Allowed'             => 'div,b,strong,i,em,pre,a[href|title|target],ul,ol,li,p[style],br,span[style],img[width|height|alt|src|style|class],table[class|width],td,tr',
+            'HTML.Allowed'             => 'div,b,font[color|style],strong,i,em,pre,a[href|title|target],ul,ol,li,p[style],br,span[style],img[width|height|alt|src|style|class],table[class|width],td,tr',
             'CSS.AllowedProperties'    => 'font,font-size,width,height,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
             'Attr.AllowedFrameTargets' =>'_blank',
             'AutoFormat.AutoParagraph' => true,
