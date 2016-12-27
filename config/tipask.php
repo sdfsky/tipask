c3596757aaaedaf3925d06d6018beabb2b6aa304
宋登峰 <sky_php@qq.com>
2016-12-27 15:58:37 +0800

    版本号更新

--- config/tipask.php
+++ config/tipask.php
@@ -8,7 +8,7 @@
 
 return [
     'version' => 'Tipask3.1',
-    'release' => '201601210',
+    'release' => '201601227',
     'user_cache_time' => 1, //用户数据缓存时间单位分钟
     'user_invite_limit' => 10,//用户邀请回答限制数
     'admin' => [
