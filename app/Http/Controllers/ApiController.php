46c6b376df7c3fcee9feebfa0426661b7c07b025
宋登峰 <sky_php@qq.com>
2016-12-28 13:36:30 +0800

    积分记录模块完善

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -18,17 +18,17 @@ class ApiController extends Controller
 
     public function getList(){
 
-        $hotQuestions = Cache::remember('hot_questions',10,function() {
-            return  Question::hottest(6);
+        $hotQuestions = Cache::remember('tipask_hot_questions_',10,function() {
+            return  Question::hottest(0,6);
         });
 
         /*悬赏问题*/
-        $rewardQuestions = Cache::remember('reward_questions',10,function() {
-            return  Question::reward(8);
+        $rewardQuestions = Cache::remember('tipask_reward_questions',10,function() {
+            return  Question::reward(0,8);
         });
 
-        $hotArticles = Cache::remember('hot_articles',10,function() {
-            return  Article::hottest(8);
+        $hotArticles = Cache::remember('tipask_articles',10,function() {
+            return  Article::hottest(0,8);
         });
 
         return response()->json([
